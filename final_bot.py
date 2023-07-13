935233dc328a7564b10ffd8f9aedf51fef64eacb
IvanMorozov2006 <ivanmorozov2006@list.ru>
2023-07-13 14:32:46 +0300

    final version

--- final_bot.py
+++ final_bot.py
@@ -4,15 +4,16 @@ import random
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 
-
 bot = Bot(token=Token)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-def update_game_board(row, col, player):
+def update_game_board(row, col, player, game_board):
     game_board[row][col] = player
+    return game_board
 
-def is_game_over():
+
+def is_game_over(game_board):
     for row in game_board:
         if row.count(row[0]) == len(row) and row[0] != " ":
             if row[0] == 'X':
@@ -21,13 +22,15 @@ def is_game_over():
                 return 'O'
 
     for col in range(len(game_board[0])):
-        if all(game_board[row][col] == game_board[0][col] and game_board[0][col] != " " for row in range(len(game_board))):
+        if all(game_board[row][col] == game_board[0][col] and game_board[0][col] != " " for row in
+               range(len(game_board))):
             if game_board[0][col] == 'X':
                 return 'X'
             else:
                 return 'O'
 
-    if (game_board[0][0] == game_board[1][1] == game_board[2][2] != " ") or (game_board[0][2] == game_board[1][1] == game_board[2][0] != " "):
+    if (game_board[0][0] == game_board[1][1] == game_board[2][2] != " ") or (
+            game_board[0][2] == game_board[1][1] == game_board[2][0] != " "):
         if game_board[1][1] == 'X':
             return 'X'
         else:
@@ -39,10 +42,8 @@ def is_game_over():
             huy = True
             break
 
-
     if huy == False:
         return ' '
-
 
     return 'no'
 
@@ -50,13 +51,15 @@ def is_game_over():
 @dp.callback_query_handler(lambda query: query.data.startswith('move'), state='*')
 async def make_move(callback_query: types.CallbackQuery, state: FSMContext):
     row, col = map(int, callback_query.data.split(':')[1].split(','))
-    global s
-    if s =='no':
+    data = await state.get_data()
+    s = data['s']
+    game_board = data['game_board']
+    if s == 'no':
         if game_board[row][col] == ' ':
             player = "X"
             bt = "O"
 
-            update_game_board(row, col, player)
+            game_board = update_game_board(row, col, player, game_board)
             huy = False
             for i in range(3):
                 if ' ' in game_board[i]:
@@ -69,7 +72,7 @@ async def make_move(callback_query: types.CallbackQuery, state: FSMContext):
                 while game_board[row][col] != ' ':
                     row = random.randint(0, 2)
                     col = random.randint(0, 2)
-                update_game_board(row, col, bt)
+                game_board = update_game_board(row, col, bt, game_board)
 
             markup = types.InlineKeyboardMarkup(row_width=3)
             for i in range(3):
@@ -79,11 +82,13 @@ async def make_move(callback_query: types.CallbackQuery, state: FSMContext):
                     button_callback = f"move:{i},{j}"
                     buttons_row.append(types.InlineKeyboardButton(text=button_text, callback_data=button_callback))
                 markup.row(*buttons_row)
-
+            await state.update_data(game_board=game_board)
             # Edit the message with the updated game board
-            await bot.edit_message_reply_markup(callback_query.message.chat.id, callback_query.message.message_id, reply_markup=markup)
+            await bot.edit_message_reply_markup(callback_query.message.chat.id, callback_query.message.message_id,
+                                                reply_markup=markup)
 
-            s = is_game_over()
+            s = is_game_over(game_board)
+            await state.update_data(s=s)
             if s != 'no':
                 winner = s
                 if winner == " ":
@@ -96,10 +101,13 @@ async def make_move(callback_query: types.CallbackQuery, state: FSMContext):
     else:
         await bot.send_message(callback_query.message.chat.id, 'Игра закончилась!')
 
+
 @dp.message_handler(commands=['start'], state='*')
 async def start(message: types.Message, state: FSMContext):
-    await state.update_data(game_board=[[' ']*3 for i in range(3)])
+    await state.update_data(game_board=[[' '] * 3 for i in range(3)])
     await state.update_data(s='no')
+    data = await state.get_data()
+    game_board = data['game_board']
     markup = types.InlineKeyboardMarkup(row_width=3)
     for i in range(3):
         buttons_row = []
@@ -111,4 +119,6 @@ async def start(message: types.Message, state: FSMContext):
 
     await bot.send_message(message.chat.id, "Игра началась.", reply_markup=markup)
 
+
 executor.start_polling(dp, skip_updates=True)
+
